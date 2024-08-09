609bd098d8d8b99ec447c0ee79ab7b816e0ebaa3
Laureano Juarez <laureanojuarez69@gmail.com>
2024-08-09 19:04:27 -0300

    revise algunos errores

--- javascript-e1/main.js
+++ javascript-e1/main.js
@@ -1,6 +1,6 @@
-// Ejercicio 1
+// 1- Crear una función que reciba un número por parámetro e indique en consola si el número es par o impar.
 
-function parImpar(num) {
+function ej1(num) {
   if (num % 2 === 0) {
     console.log("El numero que ingresaste es par");
   } else {
@@ -8,9 +8,9 @@ function parImpar(num) {
   }
 }
 
-// Ejercicio 2
+// 2- Crear una función que reciba dos números por parámetro e indique en consola que número es mayor, y si ninguno lo es, indicar por consola que son iguales.
 
-function mayorMenor(num1, num2) {
+function ej2(num1, num2) {
   if (num1 > num2) {
     console.log("El", num1, "es mayor");
   } else if (num2 > num1) {
@@ -20,17 +20,19 @@ function mayorMenor(num1, num2) {
   }
 }
 
-// Ejercicio 3
+// 3- Crear una función que reciba un número por parámetro e indique en consola si ese número es múltiplo de 5.
 
-function multiplo5(num) {
+function ej3(num) {
   if (num % 5 === 0) {
-    console.log("Es multiplo de 5");
+    console.log(`el ${num} es multiplo de 5`);
   } else {
-    console.log("no es multiplo de 5");
+    console.log(`El ${num} no es multiplo de 5`);
   }
 }
 
-// Ejercicio 4
+// ej3(15);
+
+// 4- Crear una función que reciba un número por parámetro e imprima por consola todos los números desde el 0 hasta llegar a ese número.
 
 function ej4(numero) {
   for (let i = 0; i <= numero; i++) {
@@ -38,7 +40,9 @@ function ej4(numero) {
   }
 }
 
-// Ejercicio 5
+// ej4(7);
+
+// 5 - Crear una función que reciba una palabra y un número por parámetro e imprima por consola  esa palabra la cantidad correspondiente al número indicado.
 
 function ej5(palabra, numero) {
   for (let i = 1; i <= numero; i++) {
@@ -46,7 +50,7 @@ function ej5(palabra, numero) {
   }
 }
 
-// Ejercicio 6
+// 6 - Crear una función que reciba un array por parámetro e imprima por consola todos los valores de ese array.
 
 const array = [3, 6, 9, 12, 16];
 
@@ -56,7 +60,7 @@ function ej6(array) {
   }
 }
 
-// Ejercicio 7
+// 7 - Crear una función que reciba un array con 10 números e imprima por consola todos los valores de ese array, menos el que se encuentre en la 5ta posición del mismo. Ayuda: Recuerden que el primer índice de un array es "0".
 
 const array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function ej7(array2) {
@@ -66,3 +70,15 @@ function ej7(array2) {
     }
   }
 }
+
+// 8 - Crea una función que reciba un array de números y un número por parámetro e imprima por consola cada número del array multiplicado por el número pasado por parámetro.
+
+const array3 = [4, 2, 7, 9, 3];
+
+function ej8(array3, numero) {
+  for (let i = 0; i <= array3.length; i++) {
+    console.log(array3[i] * numero);
+  }
+}
+
+// ej8(array3, 2);
