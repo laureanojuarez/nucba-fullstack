c418d776fd22fc4f13c35483e970800c4f5f49f8
Laureano <laureanojuarez69@gmail.com>
2024-09-20 04:38:10 -0300

    Agregue el local storage

--- javascript-e3/main.js
+++ javascript-e3/main.js
@@ -1,6 +1,20 @@
 const productContainer = document.querySelector(".products-container");
 const boton = document.getElementById("boton");
 
+// LOCAL STORAGE
+const guardarStorage = (value) => {
+  localStorage.setItem("pizzaId", value);
+};
+
+const cargarStorage = () => {
+  const savedPizzaId = localStorage.getItem("pizzaId");
+  if (savedPizzaId) {
+    document.getElementById("inputNumero").value = savedPizzaId;
+  }
+};
+
+// RENDERIZAR LOS PRODUCTOS
+
 const renderProducts = () => {
   boton.addEventListener("click", () => {
     const inputNumero = document.getElementById("inputNumero").value;
@@ -12,14 +26,16 @@ const renderProducts = () => {
       return;
     }
 
+    guardarStorage(inputNumero);
+
     const pizza = pizzas.find((e) => e.id == inputNumero);
 
     if (pizza) {
       productContainer.innerHTML = `
       <div class="pizza-card">
-          <h2>${pizza.nombre}</h2>
-          <img src="${pizza.imagen}" alt="${pizza.nombre}">
-          <p>Precio: ${pizza.precio}</p>
+      <h2>${pizza.nombre}</h2>
+      <img src="${pizza.imagen}" alt="${pizza.nombre}">
+      <p>Precio: ${pizza.precio}</p>
       </div>
       `;
     } else {
@@ -31,6 +47,7 @@ const renderProducts = () => {
 };
 
 const init = () => {
+  cargarStorage();
   renderProducts();
 };
 
