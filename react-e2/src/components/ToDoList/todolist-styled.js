8b3a246796f1afd72ecbb395a5e578056d10b7d2
Laureano <laureanojuarez69@gmail.com>
2024-12-17 23:05:17 -0300

    Mejore los estilos mas fiel al original

--- react-e2/src/components/ToDoList/todolist-styled.js
+++ react-e2/src/components/ToDoList/todolist-styled.js
@@ -8,4 +8,13 @@ export const ToDoListStyled = styled.section`
   gap: 25px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   padding: 20px;
+  border-radius: 10px;
+  background-color: #ffffff;
+
+  @media (max-width: 768px) {
+    width: 450px;
+  }
+  @media (max-width: 425px) {
+    width: 350px;
+  }
 `;
--- react-e2/src/styles/GlobalStyles.js
+++ react-e2/src/styles/GlobalStyles.js
@@ -12,5 +12,5 @@ body {
     justify-content: center;
     align-items: center;
     height: 100vh;
-}
+    background-color: #f0f0f0;}
 `;
